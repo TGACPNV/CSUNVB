90d881fd515be725ffe4a81568c5a8d25e52cd98
tazuwqwq <tazuwqwq@firemail.cc>
2021-01-08 11:55:28 +0100

    improvements

--- public/js/drugs.js
+++ public/js/drugs.js
@@ -5,17 +5,20 @@
 
 function novaCheck(novaID, drugID, dateID) {
     divID = novaID + drugID + dateID;
-    quantityCheck(divID);
+    let originalQuantity = document.getElementById(divID + "start").value;
+    let currentQuantity = document.getElementById(divID + "end").value;
+    quantityCheck(divID, originalQuantity, currentQuantity);
 }
 
 function pharmaCheck(drugID, dateID) {
     divID = drugID + dateID;
-    quantityCheck(divID);
+    let originalQuantity = document.getElementById(divID + "start").value;
+    let currentQuantity = document.getElementById(divID + "end").value; // + document.getElementById(divID + "day").value + document.getElementById(divID + "night").value;
+    quantityCheck(divID, originalQuantity, currentQuantity);
 }
 
-function quantityCheck(divID) {
-    let originalQuantity = document.getElementById(divID + "start").value;
-    let currentQuantity = document.getElementById(divID + "end").value;
+function quantityCheck(divID, originalQuantity, currentQuantity) {
+
     if(Number(currentQuantity) !== Number(originalQuantity)) {
         document.getElementById(divID).style = "background-color: orange;"
     } else {
