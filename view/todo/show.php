212105d153175f34686976ac1470d71827637ff6
GAMPER Daniel <Daniel.GAMPER@cpnv.ch>
2021-01-15 11:58:56 +0100

    Update bouton Modification des taches
    Changement du bouton pour plus de clarté

--- view/todo/show.php
+++ view/todo/show.php
@@ -36,11 +36,19 @@ $title = "CSU-NVB - Tâches hebdomadaires";
     </div>
     <div class="d-flex flex-row"> <!-- Boutons relatifs à l'état de la feuille -->
         <?php if(ican ("modifySheet") && $week['slug'] == "blank") : ?>
-        <form action="?action=modTemplate" method="POST">
-            <input type="hidden" name="todosheetID" value="<?= $week['id'] ?>">
-            <input type="hidden" name="edition" value="<?= $edition ?>">
-            <button type="submit" class='btn btn-primary m-1 float-right'>Modification des tâches</button>
-        </form>
+            <?php if($edition == false) : ?>
+                <form action="?action=modTemplate" method="POST">
+                    <input type="hidden" name="todosheetID" value="<?= $week['id'] ?>">
+                    <input type="hidden" name="edition" value="<?= $edition ?>">
+                    <button type="submit" class='btn btn-primary m-1 float-right'>Mode édition</button>
+                </form>
+            <?php else:?>
+                <form action="?action=modTemplate" method="POST">
+                    <input type="hidden" name="todosheetID" value="<?= $week['id'] ?>">
+                    <input type="hidden" name="edition" value="<?= $edition ?>">
+                    <button type="submit" class='btn btn-primary m-1 float-right'>Quitter édition</button>
+                </form>
+            <?php endif; ?>
         <?php endif; ?>
         <?=  slugsButtonTodo($week['slug'], $week['id'])?>
     </div>
