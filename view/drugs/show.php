fcbdc4684059af19149c2257876e9f27c98caf0e
tazuwqwq <tazuwqwq@firemail.cc>
2021-01-19 16:04:59 +0100

    merge changes

--- view/drugs/show.php
+++ view/drugs/show.php
@@ -12,7 +12,8 @@ ob_start();
     <h1>Stupéfiants</h1>
     <h2>Semaine <?= $drugsheet["week"] ?> - Base de <?= $site['name'] ?> [<?= $drugsheet['displayname'] ?>]</h2>
     <div class="d-flex justify-content-end d-print-none">
-        <button type='submit' class='btn btn-primary m-1 float-right' onclick="window.print()">Télécharger en PDF</button>
+        <button type='submit' class='btn btn-primary m-1 float-right' onclick="window.print()">Télécharger en PDF
+        </button>
         <form>
             <input type="hidden" name="action" value="listDrugSheets">
             <input type="hidden" name="id" value="<?= $site['id'] ?>">
@@ -50,62 +51,43 @@ ob_start();
                 <td></td>
                 <?php foreach ($novas as $nova): ?>
                     <?php $ncheck = getNovaCheckByDateAndDrug($date, $drug['id'], $nova['id'], $drugsheet['id']); // not great practice, but it spares repeated queries on the db ?>
-                    <td id="<?= $nova["number"] . $drug["name"] . $date ?>">
+                    <?php $UID = $nova["number"] . $drug["name"] . $date ?>
+                    <td id="<?= $UID ?>">
                         <input type="number" min="0" width="30" height="100" class="text-center"
                                value="<?= $ncheck ? $ncheck["start"] : '' ?>"
-                               onchange="novaCheck(<?= "'" . $nova["number"] . "', '" . $drug["name"] . "', '" . $date . "'" ?>);"
-                               id="<?= $nova["number"] . $drug["name"] . $date ?>start">
+                               onchange="cellUpdate(<?= "'" . $UID . "'" ?>);"
+                               id="<?= $UID ?>start">
                         <input type="number" min="0" width="20" height="30" class="text-center"
                                value="<?= $ncheck ? $ncheck["end"] : '' ?>"
-                               onchange="novaCheck(<?= "'" . $nova["number"] . "', '" . $drug["name"] . "', '" . $date . "'" ?>);"
-                               id="<?= $nova["number"] . $drug["name"] . $date ?>end">
+                               onchange="cellUpdate(<?= "'" . $UID . "'" ?>);"
+                               id="<?= $UID ?>end">
                     </td>
                 <?php endforeach; ?>
                 <td></td>
             </tr>
-            <?php foreach ($batchesByDrugId[$drug["id"]] as $batch): ?>
-                <?php $pcheck = getPharmaCheckByDateAndBatch($date, $batch['id'], $drugsheet['id']); // not great practice, but  it spares repeated queries on the db ?>
-                <tr>
-                    <td class="text-right"><?= $batch['number'] ?></td>
-                    <td class="text-center">
-                        <input id='<?= $drug["name"] . $date . "start" ?>' type="number" min="0" width="30" height="100"
-                               class="text-center" value="<?= $pcheck ? $pcheck['start'] : '' ?>"
-                               onchange="pharmaCheck(<?= "'" . $drug["name"] . "', '" . $date . "'" ?>);"
-                               id="<?= $nova["number"] . $drug["name"] . $date ?>start">
-                    </td>
-                    <?php foreach ($novas as $nova): ?>
-                        <?php $UID =  $nova["number"] . $drug["name"] . $date ?>
-                        <?php $ncheck = getNovaCheckByDateAndDrug($date, $drug['id'], $nova['id'], $drugsheet['id']); // not great practice, but it spares repeated queries on the db ?>
-                        <td id="<?= $UID ?>">
-                            <input type="number" min="0" class="text-center" value="<?= $ncheck ? $ncheck["start"] : ''?>" onchange="cellUpdate(<?= "'" . $UID . "'" ?>);" id="<?= $UID ?>start">
-                            <input type="number" min="0" class="text-center" value="<?= $ncheck ? $ncheck["end"] : '' ?>" onchange="cellUpdate(<?= "'" . $UID . "'" ?>);" id="<?= $UID ?>end">
-                        </td>
-                    <?php endforeach; ?>
-                    <td id='<?= $drug["name"] . $date ?>' class="text-center">
-                        <input id='<?= $drug["name"] . $date . "end" ?>' type="number" min="0" width="30" height="100"
-                               class="text-center" value="<?= $pcheck ? $pcheck['end'] : '' ?>"
-                               onchange="pharmaCheck(<?= "'" . $drug["name"] . "', '" . $date . "'" ?>);"
-                               id="<?= $nova["number"] . $drug["name"] . $date ?>start">
-                    </td>
-                </tr>
                 <?php foreach ($batchesByDrugId[$drug["id"]] as $batch): ?>
                     <?php $UID = "pharma" . $drug["name"] . $date ?>
                     <?php $pcheck = getPharmaCheckByDateAndBatch($date, $batch['id'], $drugsheet['id']); // not great practice, but  it spares repeated queries on the db ?>
                     <tr>
                         <td class="text-right"><?= $batch['number'] ?></td>
                         <td class="text-center">
-                            <input id="<?= $UID ?>start" type="number" min="0" value="<?= $pcheck ? $pcheck['start'] : '' ?>" onchange="cellUpdate(<?= "'" . $UID . "'"?>);">
+                            <input id="<?= $UID ?>start" type="number" min="0"
+                                   value="<?= $pcheck ? $pcheck['start'] : '' ?>"
+                                   onchange="cellUpdate(<?= "'" . $UID . "'" ?>);">
                         </td>
                         <?php foreach ($novas as $nova): ?>
                             <td class="text-center">
-                                <input class="<?= $UID ?> nova" type="number" min="0" value="<?= getRestockByDateAndDrug($date,$batch['id'],$nova['id']) ?>" onchange="cellUpdate(<?= "'" . $UID . "'"?>);">
+                                <input class="<?= $UID ?> nova" type="number" min="0"
+                                       value="<?= getRestockByDateAndDrug($date, $batch['id'], $nova['id']) ?>"
+                                       onchange="cellUpdate(<?= "'" . $UID . "'" ?>);">
                             </td>
                         <?php endforeach; ?>
                         <td id="<?= $UID ?>" class="text-center">
-                            <input id="<?= $UID ?>end" type="number" min="0" value="<?= $pcheck ? $pcheck['end'] : '' ?>" onchange="cellUpdate(<?= "'" . $UID . "'"?>);">
+                            <input id="<?= $UID ?>end" type="number" min="0"
+                                   value="<?= $pcheck ? $pcheck['end'] : '' ?>"
+                                   onchange="cellUpdate(<?= "'" . $UID . "'" ?>);">
                         </td>
                     </tr>
-                <?php endforeach; ?>
             <?php endforeach; ?>
         <?php endforeach; ?>
         <tr>
