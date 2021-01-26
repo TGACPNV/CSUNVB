dfde22f019f3c6182c16d50d87b8ef263ed2d94d
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-01-26 13:02:23 +0100

    New admin home page

--- view/admin/adminHome.php
+++ view/admin/adminHome.php
@@ -3,11 +3,10 @@ ob_start();
 $title = "CSU-NVB - Administration";
 ?>
 <div class="row m-4 d-flex justify-content-center">
-    <a class="col-4 bigfont btn btn-primary btn-large p-5 m-5 font-weight-bolder" href="?action=adminCrew">Secouristes</a>
-    <a class="col-4 bigfont btn btn-primary btn-large p-5 m-5 font-weight-bolder" href="?action=adminBases">Bases</a>
-    <a class="col-4 bigfont btn btn-primary btn-large p-5 m-5 font-weight-bolder" href="?action=adminNovas">Ambulances</a>
-    <a class="col-4 bigfont btn btn-primary btn-large p-5 m-5 font-weight-bolder" href="?action=adminDrugs">Médicaments</a>
-
+    <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=adminCrew"><i class="fas fa-users fa-8x"></i><br>Secouristes</a>
+    <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=adminBases"><i class="fas fa-hospital fa-8x"></i><br>Bases</a>
+    <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=adminNovas"><i class="fas fa-ambulance fa-8x"></i><br>Ambulances</a>
+    <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=adminDrugs"><i class="fas fa-syringe fa-8x"></i><br>Médicaments</a>
 </div>
 <?php
 $content = ob_get_clean();
