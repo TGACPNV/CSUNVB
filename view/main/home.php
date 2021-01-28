c24f3dac3b2cc78dd3e9be474a770f9ef74b29f0
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-01-28 11:15:44 +0100

    cosmetics

--- view/main/home.php
+++ view/main/home.php
@@ -12,12 +12,12 @@ $title = "CSU-NVB - Accueil";
 
 <div class="container ">
     <div class="row m-4 d-flex justify-content-center">
+        <a class="col-4 text-decoration-none text-black-50 text-center p-5" href="?action=listshift"><i class="fas fa-file-signature fa-8x"></i><br>Remise de garde</a>
+        <a class="col-4 text-decoration-none text-black-50 text-center p-5" href="?action=listtodo"><i class="far fa-check-circle fa-8x"></i><br>Tâches hebdomadaires</a>
+        <a class="col-4 text-decoration-none text-black-50 text-center p-5" href="?action=listDrugSheets"><i class="fas fa-syringe fa-8x"></i><br>Stupéfiants</a>
         <?php if ($_SESSION['user']['admin'] == true): ?>
-            <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=adminHome"><i class="fas fa-users-cog fa-8x"></i><br>Administration</a>
+            <a class="col-4 text-decoration-none text-black-50 text-center p-5" href="?action=adminHome"><i class="fas fa-users-cog fa-8x"></i><br>Administration</a>
         <?php endif; ?>
-        <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=listshift"><i class="fas fa-file-signature fa-8x"></i><br>Remise de garde</a>
-        <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=listtodo"><i class="far fa-check-circle fa-8x"></i><br>Tâches hebdomadaires</a>
-        <a class="col-4 text-decoration-none text-black-50 text-center m-5 p-5" href="?action=listDrugSheets"><i class="fas fa-syringe fa-8x"></i><br>Stupéfiants</a>
     </div>
 
 </div>
